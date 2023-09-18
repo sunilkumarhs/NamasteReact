818f7eaec3df6d377e24f65892bff56725ddaaeb
sunilkumarhs <sunilkumarhs974117@gmail.com>
2023-09-18 22:04:49 +0530

    episode 2  learning of Namste React

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1", { id: "head", xyz: "abcd" }, [
   "Hello World from React!",
   "Hello World from React!",
@@ -5,12 +8,12 @@ const heading = React.createElement("h1", { id: "head", xyz: "abcd" }, [
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1", key: "p1" }, [
-    React.createElement("h1", { key: "c1.1" }, "This is h1 tag!"),
-    React.createElement("h2", { key: "c1.2" }, "This is h2 tag!"),
+    React.createElement("h1", { key: "c1.1" }, "This is Namaste React"),
+    React.createElement("h2", { key: "c1.2" }, "This is h3 tag!"),
   ]),
   React.createElement("div", { id: "child2", key: "p2" }, [
-    React.createElement("h1", { key: "c2.1" }, "This is h1 tag!"),
-    React.createElement("h2", { key: "c2.2" }, "This is h2 tag!"),
+    React.createElement("h1", { key: "c2.1" }, "This is Namaste React"),
+    React.createElement("h2", { key: "c2.2" }, "This is Sunil Kumar H S"),
   ]),
 ]);
 console.log(parent);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 #Namaste React🔗
+
+
+#Parcel
+-Dev Build
+-Local Server
+-HMR = Hot Module Replacement
+-File Watching Algorithm = written in c++
+-Caching = Faster Building
+-Image Optimization
+-Minification
+-Bundling
+-Compress
+-Consistent Hashing
+-Code Spliting
+-Differential Bundling = support older browsers
+-Diagnostic
+-Error Handling
+-HTTPs
+-Tree Shaking = remove unwanted code
+-Different dev and prod bundles
