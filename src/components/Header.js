d984716b9a863056db084061e67f2ca398bd7b60
sunilkumarhs <sunilkumarhs974117@gmail.com>
2023-10-10 08:24:49 +0530

    tailwindcss headetr

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,18 +7,18 @@ const Header = () => {
   const [loginCredential, setLoginCredential] = useState("SignIn");
   const onlineStatus = useOnlineStatus();
   return (
-    <div className="header">
-      <div className="logo">
-        <img src={picture} alt="logo" />
+    <div className="flex justify-between px-3 py-3 bg-amber-300 rounded-xl mx-4 my-2">
+      <div>
+        <img src={picture} alt="logo" className=" rounded-full" />
       </div>
-      <div className="title">
-        <h1 id="heading" className="heading">
+      <div className=" bg-red-400 px-3 rounded-2xl text-center">
+        <h1 className="text-lg font-bold text-white py-4">
           React Food Dilivery App
         </h1>
       </div>
-      <div className="NavItems">
-        <ul>
-          <li>
+      <div className="">
+        <ul className="flex justify-between px-3 py-3 text-lg cursor-pointer">
+          <li className=" px-2 mx-2">
             <button
               style={{
                 backgroundColor: `${
@@ -28,17 +28,19 @@ const Header = () => {
                 borderRadius: "2rem",
                 border: "none",
               }}
-              className="tooltip"
+              className=" hover:visible relative inline-block "
             >
-              <span className="tooltiptext">Online Status</span>
+              <span className=" invisible hover:visible text-sm w-30 bg-white text-black rounded-2xl px-2 absolute z-10 bottom-full left-1/2 -ml-14 ">
+                Online Status
+              </span>
             </button>
           </li>
-          <li>
+          <li className=" px-2 py-2 mx-2 hover:text-xl hover:border-b-4 hover:border-white">
             <Link to="/" style={{ textDecoration: "none", color: "black" }}>
               Home
             </Link>
           </li>
-          <li>
+          <li className=" px-2 py-2 mx-2 hover:text-xl hover:border-b-4 hover:border-white">
             <Link
               to="/about"
               style={{ textDecoration: "none", color: "black" }}
@@ -46,7 +48,7 @@ const Header = () => {
               About Us
             </Link>
           </li>
-          <li>
+          <li className=" px-2 py-2 mx-2 hover:text-xl hover:border-b-4 hover:border-white">
             <Link
               to="/contact"
               style={{ textDecoration: "none", color: "black" }}
@@ -54,13 +56,13 @@ const Header = () => {
               Contact Us
             </Link>
           </li>
-          <li>
+          <li className=" px-2 py-2 mx-2 hover:text-xl hover:border-b-4 hover:border-white">
             <Link to="/cart" style={{ textDecoration: "none", color: "black" }}>
               Cart
             </Link>
           </li>
           <button
-            className="sign-btn"
+            className=" px-4 py-2 mx-2 rounded-xl text-xl hover:bg-red "
             style={{
               backgroundColor: `${
                 loginCredential === "SignIn" ? "lightGreen" : "red"
