d334690c3a20effbd9d729b0a930a9025d471736
sunilkumarhs <sunilkumarhs974117@gmail.com>
2024-04-03 11:19:34 +0530

    "api update"

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,10 +2,10 @@ export const CDN_IMGLINK =
   "https://media-assets.swiggy.com/swiggy/image/upload/";
 
 export const RES_LINK =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?&lat=13.0169992&lng=77.7044335&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://thingproxy-760k.onrender.com/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?&lat=13.0169992&lng=77.7044335&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const RESMENU_LINK =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9652603&lng=77.5759163&restaurantId=";
+  "https://thingproxy-760k.onrender.com/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9652603&lng=77.5759163&restaurantId=";
 
 export const FOOD_IMG = "https://media-assets.swiggy.com/swiggy/image/upload/";
 export const LICENSE_LOGO =
@@ -14,7 +14,7 @@ export const LICENSE_LOGO =
 export const GIT_HUB = " https://api.github.com/users/";
 
 export const COLLECTION_LINK_START =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0169992&lng=77.7044335&collection=";
+  "https://thingproxy-760k.onrender.com/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0169992&lng=77.7044335&collection=";
 
 export const COLLECTION_LINK_END =
   "&tags=layout_ux6&sortBy=&filters=&type=rcv2&offset=0&page_type=null";
